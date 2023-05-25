46b922dd77d7e2bbb29613998b773c32baa71852
Malinali Becerril <mali@devf.mx>
2023-05-25 10:28:56 -0600

    add if to github

--- 2.1.2_condicionales/script.js
+++ 2.1.2_condicionales/script.js
@@ -21,11 +21,11 @@ true
 "123455" === prompt('pon tu contraseña')
 false
 if ("123455" === prompt('pon tu contraseña')) {
-    alert("Bienvenido, ya estamos adentro.")
+    alert("Bienvenido, ya estamos adentro.");
 }
 undefined
 if ("123455" === prompt('pon tu contraseña')) {
-    alert("Bienvenido, ya estamos adentro.")
+    alert("Bienvenido, ya estamos adentro.");
 }
 undefined
 if ("123455" === prompt('pon tu contraseña')) {
@@ -35,4 +35,54 @@ if ("123455" === prompt('pon tu contraseña')) {
 }
 
 
+*/
+/**
+    
+let contraseniaIngresada = prompt('pon tu contraseña');
+
+if ("123455" === contraseniaIngresada) {
+    alert("Bienvenida Mali, ya estamos adentro.");
+} else if ("asdfg" ===contraseniaIngresada) {
+    alert("Bienvenido Alfredo, ya estamos adentro.");
+} else {
+    console.error("ALARMA: quieren robar mi cuenta.");
+}
+
+if ("123455" === prompt('pon tu contraseña') && 18 <= parseInt(prompt("dame tu edad"))) {
+    alert("Bienvenido, ya estamos adentro.");
+} else {
+    console.error("ALARMA: MENOR DE EDAD quiere robar mi cuenta.");
+}
+
+if ( 20 <= prompt('cuanto traes') || 18 <= parseInt(prompt("dame tu edad"))) {
+    alert("Bienvenido, ya estamos adentro.");
+} else {
+    document.write('<h1 style="color:red;">ALARMA: MENOR DE EDAD quiere colarse al bar.</h1>');
+    console.error("ALARMA: MENOR DE EDAD quiere colarse al bar.");
+}
+
+
+
+Solicitar al usuario que responda a la pregunta (“¿Eres bellisimo/a?”), en
+caso de contestar sí, responder “Ciertamente”, en caso de contestar no,
+responder: “No te creo”.
+
+ */
+
+let respuestaEjercicio1 = prompt("¿Eres bellisimo/a?");
+
+if (respuestaEjercicio1 === "si" || respuestaEjercicio1 === "Si" 
+    || respuestaEjercicio1 === "SI") {
+    console.log("Ciertamente.");
+} else if (respuestaEjercicio1 === "no") {
+    console.log("No te creo.");
+} else {
+    console.log("Eso que escribiste no lo entiendo.");
+}
+
+/* 
+let variableText = prompt("pregunta");
+let variableNumero = parseFloat(variableText);
+
+let variableEnNumero = parseFloat(prompt("pregunta")); 
 */
