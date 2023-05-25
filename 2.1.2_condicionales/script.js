46a4c026bbd13e67370feebe7a737387a3cc351d
Malinali Becerril <mali@devf.mx>
2023-05-24 19:12:13 -0600

    add ejemplos de if

--- 2.1.2_condicionales/script.js
+++ 2.1.2_condicionales/script.js
@@ -1,7 +1,38 @@
-// let nombre; -> undefined un valor no controlado que no existe
+/* // let nombre; -> undefined un valor no controlado que no existe
 // let nombre = null; porque estoy dejando sin saber de que tipo seria mi variable
 let nombre = "";
-console.log(`El valor de nombre antes del prompt: ${nombre}.`);
+console.log("El valor de nombre antes del prompt: " + nombre + ".");
+console.log(`El valor de nombre antes
+ del prompt: ${nombre}.`);
 nombre = prompt("Ingresa tu nombre");
-console.log(`El nombre ingresado es: ${nombre}.`);
+console.log(`El nombre ingresado es: ${nombre}.`); */
 
+
+/*
+
+1 === 1.0
+true
+"1" == 1.0
+true
+"123455" = prom
+VM685:1 Uncaught SyntaxError: Invalid left-hand side in assignment
+"123455" === prompt('pon tu contraseña')
+true
+"123455" === prompt('pon tu contraseña')
+false
+if ("123455" === prompt('pon tu contraseña')) {
+    alert("Bienvenido, ya estamos adentro.")
+}
+undefined
+if ("123455" === prompt('pon tu contraseña')) {
+    alert("Bienvenido, ya estamos adentro.")
+}
+undefined
+if ("123455" === prompt('pon tu contraseña')) {
+    alert("Bienvenido, ya estamos adentro.");
+} else {
+    console.error("ALARMA: MENOR DE EDAD quiere robar mi cuenta");
+}
+
+
+*/
