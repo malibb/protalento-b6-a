e2d76f01b025859c6bafff0780a0f3fb7cb7da3d
Malinali Becerril <mali@devf.mx>
2023-06-28 18:14:34 -0600

    node - npm

---
+++ 3.1.2_npm/ejercicio1.js
@@ -0,0 +1,10 @@
+let respuestaEjercicio1 = "si";
+
+if (respuestaEjercicio1 === "si" || respuestaEjercicio1 === "Si" 
+    || respuestaEjercicio1 === "SI") {
+    console.log("Ciertamente.");
+} else if (respuestaEjercicio1 === "no") {
+    console.log("No te creo.");
+} else {
+    console.log("Eso que escribiste no lo entiendo.");
+}
---
+++ 3.1.2_npm/index.js
@@ -0,0 +1,8 @@
+const PI = 3.1416;
+
+const r = 2;
+
+let areaCirculo = PI * (r**2);
+
+console.log(areaCirculo);
+
