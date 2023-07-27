b0fa57ca431a861527cdc914c8484eeec61748e7
Malinali Becerril <mali@devf.mx>
2023-07-26 20:00:11 -0600

    add first endpoint

--- 4.1.1_rest/app.js
+++ 4.1.1_rest/app.js
@@ -1,1 +1,11 @@
-console.log("Hola");
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Aplicación de ejemplo en el puerto: ${port}`);
+});
