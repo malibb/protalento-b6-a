31f2722c7f57718d090c16040f01148fe925bce9
Guirdo <produccionesaldo@gmail.com>
2023-08-16 18:31:16 -0600

    Add ClientController

--- 4.4.1_backend/controllers/ClientController.js
+++ 4.4.1_backend/controllers/ClientController.js
@@ -7,21 +7,55 @@ const getClients = (req, res) =>  {
         .catch(error => res.status(500).json(error));
 };
 
-const getClient = (req, res) =>  res.status(200).send({
-    message:"Clients"
-});
+const getClient = (req, res) => {
+    const { id } = req.params
+
+    clientModel
+        .getClient(id)
+        .then(results => res.status(201).json(results))
+        .catch(error => res.status(500).json(error));
+
+};
+
+const insertClient = (req, res) => {
+    const { first_name, last_name,active } = req.body
+
+    clientModel
+        .insertClient({ first_name, last_name,active })
+        .then(results => res.status(201).json(results))
+        .catch(error => res.status(500).json(error));
+}
+
+const updateClient = (req, res) => {
+    const { id } = req.params
+    const { first_name, last_name,active } = req.body
+
+    clientModel
+        .updateClient(id,{ first_name, last_name,active })
+        .then(results => res.status(201).json(results))
+        .catch(error => res.status(500).json(error));
+}
+
+const deleteClient = (req,res) => {
+    const { id } = req.params
+
+    clientModel
+        .deleteClient(id)
+        .then(results => res.status(201).json(results))
+        .catch(error => res.status(500).json(error));
+}
 
 const insertClients = (req, res) =>  res.status(200).send({
     message:"Clients"
 });
 
-const insertClient = (req, res) =>  res.status(200).send({
-    message:"Clients"
-});
+
 
 module.exports = {
     getClients,
     getClient,
     insertClients,
     insertClient,
+    updateClient,
+    deleteClient
 };
